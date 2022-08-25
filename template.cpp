0a04356bbc19f21ed9d3ca2ca4cc6d55e7c72f82
hemanth krishna <k.hemanth1999@gmail.com>
2022-08-25 19:24:42 +0530

    template for cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define debug(x) cout << #x << " "; _print(x); cout << endl;
+#define pb push_back
+
+void _print(vector<int> arr){
+    cout << "[ ";
+    for(auto it: arr){
+        cout << it <<" ";
+    }
+    cout << "]";
+}
+
+/*
+Array input:
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+*/
+void solve(){
+    
+}
+
+int32_t main(){
+    int t;
+    cin >>t;
+    while(t--){
+        solve();
+    }
+}
